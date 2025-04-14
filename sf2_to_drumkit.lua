41028fc8020c7f727f95af9d28f08d18e2f0b8a7
J.Jørgen von Bargen <bassklampfe@users.noreply.github.com>
2025-04-14 16:26:28 +0200

    use standard GM/GS names for percussion sounds

--- sf2_to_drumkit.lua
+++ sf2_to_drumkit.lua
@@ -660,6 +660,78 @@ local function data2xml(data)
 end
 
 
+
+
+local standard_midi_drum_names=
+{
+	-- GS extension
+	[27]="High Q",
+	[28]="Slap Noise",
+	[29]="Scratch Push",
+	[30]="Scratch Pull",
+	[31]="Drum sticks",
+	[32]="Square Click",
+	[33]="Metronome Click",
+	[34]="Metronome Bell",
+	-- GM standard
+	[35]="Acoustic Bass Drum",
+	[36]="Bass Drum 1",
+	[37]="Side Stick",
+	[38]="Acoustic Snare",
+	[39]="Hand Clap",
+	[40]="Electric Snare",
+	[41]="Low Floor Tom",
+	[42]="Closed High Hat",
+	[43]="High Floor Tom",
+	[44]="Pedal High Hat",
+	[45]="Low Tom",
+	[46]="Open High Hat",
+	[47]="Low Mid Tom",
+	[48]="High Mid Tom",
+	[49]="Crash Cymbal 1",
+	[50]="High Tom",
+
+	[51]="Ride Cymbal 1",
+	[52]="Chinese Cymbal",
+	[53]="Ride Bell",
+	[54]="Tambourine",
+	[55]="Splash Cymbal",
+	[56]="Cowbell",
+	[57]="Crash Cymbal 2",
+	[58]="Vibraslap",
+	[59]="Ride Cymbal 2",
+	[60]="High Bongo",
+	[61]="Low Bongo",
+	[62]="Mute High Conga",
+	[63]="Open High Conga",
+	[64]="Low Conga",
+	[65]="High Timbale",
+	[66]="Low Timbale",
+
+	[67]="High Agogo",
+	[68]="Low Agogo",
+	[69]="Cabasa",
+	[70]="Maracas",
+	[71]="Short Whistle",
+	[72]="Long Whistle",
+	[73]="Short Guiro",
+	[74]="Long Guiro",
+	[75]="Claves",
+	[76]="High Wood Block",
+	[77]="Low Wood Block",
+	[78]="Mute Cuica",
+	[79]="Open Cuica",
+	[80]="Mute Triangle",
+	[81]="Open Triangle",
+	-- GS extension
+	[82]="Shaker",
+	[83]="Jingle Bell",
+	[84]="Belltree",
+	[85]="Castanets",
+	[86]="Mute Surdo",
+	[87]="Open Surdo",
+}
+
 local function save_drumkit(SF,drumkit_dir)
 	local drumkit_name=drumkit_dir:gsub(".*/",""):gsub("drumkit%-","")
 
@@ -744,7 +816,7 @@ local function save_drumkit(SF,drumkit_dir)
 			local instrument=
 			{"instrument",
 				{"id",r},
-				{"name",sprintf("%s (%d)",sample.achSampleName,region.key_lo)},
+				{"name",sprintf("%s (%d)",standard_midi_drum_names[region.key_lo] or sample.achSampleName,region.key_lo)},
 				{"volume",1},
 				{"isMuted",false},
 				{"isSoloed",false},
